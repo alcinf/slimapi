45f31f8851e5bf41040b8d5fc93bf1ff3cae70d6
alc_inf <alc_inf@yahoo.com.mx>
2021-08-09 20:52:54 -0500

    Initial modification: Initial Route works and the simple connection.

---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+
+class DB {
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pass = '1234';
+    private $dbname = 'slim-api';
+
+    public function connect(){
+        $conn_str = "mysql:host=$this->host;dbname=$this->dbname";
+        $conn = new PDO($conn_str, $this->user, $this->pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        return $conn;
+    }
+
+
+}
+
+?>
